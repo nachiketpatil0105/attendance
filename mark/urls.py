2d0b99c620fbc10967a2c6247ea4dd76e85e7a93
nachiketpatil0105 <nachiket1729@gmail.com>
2025-07-03 19:47:56 +0530

    Tried to Fix Home button

--- mark/urls.py
+++ mark/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index"),
+  path("/", views.index, name="index"),
   path("login/", views.login_view, name="login"),
   path("logout/", views.logout_view, name="logout"),
   path("signup/", views.sign_up, name="signup"),
